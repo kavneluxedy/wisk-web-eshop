0fc8379deebd338b8eb1a95a8f204986954cc1d8
kavneluxedy <quentin.cavenel1@gmail.com>
2020-05-19 16:50:48 +0200

    Revert "Revert "Merge branch 'develop' of https://github.com/kavneluxedy/wisk-web-eshop into develop""
    This reverts commit d03d0a81e1965510d27a70355b154176f9c21ebe.

--- application/models/Store_model.php
+++ application/models/Store_model.php
@@ -12,6 +12,7 @@ class Store_model extends CI_Model
    }
 
    function getCategories()
+<<<<<<< HEAD
    {
       return $this->db->GET('wisk_shop_categories');
    }
@@ -19,6 +20,27 @@ class Store_model extends CI_Model
    function getItems()
    {
       return $this->db->GET('wisk_shop_items')->result();
+=======
+   {
+      return $this->db->GET('wisk_shop_categories');
+   }
+
+   function getItems()
+   {
+      return $this->db->GET('wisk_shop_items')->result();
+   }
+
+   function caddy($data)
+   {
+      $this->load->model('User_model');
+
+      $data = array();
+      $data['acc_id'] = $this->User_model->getUser($acc_id);
+      $data['item_name'] = $this->input->post('item_name');
+      $data['item_price'] = $this->input->post('item_price');
+
+      $this->basket->ADD($data);
+>>>>>>> 76f930e233c3c7acf37b553eea2cbfa0a6110200
    }
 
    // function basket()
--- application/views/store_list.php
+++ application/views/store_list.php
@@ -30,6 +30,7 @@
 <div class="row">
     <div class="col-md-12 col-12">
 
+<<<<<<< HEAD
         <div class="card mb-2 d-flex">
             <h5 class="card-header text-center text-warning bg-dark border-light">Ventes</h5> <!-- Conteneur de catégories -->
         </div>
@@ -41,11 +42,24 @@
         </div>
 
         <h5 class="card-title card-header text-center text-warning mt-3 mb-3 border-light bg-dark">Produits ajoutés</h5>
+=======
+        <div class="card">
+            <h5 class="card-header text-center">Ventes</h5> <!-- Conteneur de catégories -->
+            <div class="card-body d-flex flex-column">
+                <h5 class="card-title text-center align-items-start">Catégories</h5>
+                <p class="card-text text-center">Venez supporter votre équipe préférée en achetant le maillot de la team.</p>
+                <a href="<?= base_url('Store/categories'); ?>" class="btn btn-dark align-items-center justify-content-center text-warning">Découvrir</a> <!-- Lien vers différentes catégories -->
+            </div>
+        </div>
+
+        <h5 class="card-title card-header text-center mt-3 mb-3 border-dark">Produits ajoutés</h5>
+>>>>>>> 76f930e233c3c7acf37b553eea2cbfa0a6110200
         <div class="row">
             <div class="col-12 d-flex flex-row justify-content-center ">
                 <div class="card" style="width: 18rem;">
                     <!-- carte du produit -->
 
+<<<<<<< HEAD
                     <form method="POST" class="form-group" action="<?= base_url('Store/basket'); ?>">
                         <ul class="list-group list-group-flush">
 
@@ -97,6 +111,60 @@
                     </form>
 
 
+=======
+
+                    <form method="POST" class="form-group" action="<?= base_url('Store/caddy'); ?>">
+                        <ul class="list-group list-group-flush">
+
+                            <div class="card-body">
+                                <p class="card-text text-secondary">Cette carte doit pouvoir détailler le produit en question de la BDD</p>
+                            </div>
+
+                            <?php
+                            foreach ($items as $row) // Début de la lecture en boucle (Table wisk_shop_items)
+                            { ?>
+                                <li class="list-group-item d-flex flex-inline">id :
+                                    <p> <?= $row->item_id ?> </p>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-inline">Catégorie :
+                                    <p> <?= $row->cat_id ?> </p>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-inline">Nom :
+                                    <p> <?= $row->item_name ?> </p>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-inline">Description :
+                                    <p> <?= $row->item_desc ?> </p>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-inline">
+                                    <div>
+                                        <!-- TODO : Lecture d'Image en base de donnée -->
+                                        <img src="<?= base_url('assets/img/manette.jpg'); ?>" alt="Photo du produit à vendre" width="auto" height="100">
+                                    </div>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-inline">Prix :
+                                    <p> <?= $row->item_price ?> </p>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-inline">Stock :
+                                    <p> <?= $row->item_stock ?> </p>
+                                </li>
+
+                                <li class="list-group-item d-flex flex-row justify-content-center">
+                                    <input class="btn btn-dark text-warning" id="submit" type="submit" value="Ajouter au panier"></input>
+                                </li>
+
+                            <?php } // Fin de la lecture en boucle 
+                            ?>
+                        </ul>
+                    </form>
+
+
+>>>>>>> 76f930e233c3c7acf37b553eea2cbfa0a6110200
                 </div>
             </div>
         </div>
